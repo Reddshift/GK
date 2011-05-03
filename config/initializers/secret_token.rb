16e0beb01913a5838c01c723d373676b677c3a6b
Matthew Redd <mredd@ubuntu.localdomain>
2011-05-03 00:04:04 -0700

    innitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gkdemo::Application.config.secret_token = 'f05a20ba3e9ff52fb8c91e6f6a54cc7f63c9bfad4e4a0fdeb47ab0b325a616e47bc1a6a9cba3dfe6f49c582afdf3437325f1ca9f2d295a0a6fcc610672d9f68f'
---
+++ db/default/import.rb
@@ -0,0 +1,31 @@
+require 'fastercsv'
+
+prod_taxonomy = Taxonomy.find_or_create_by_name("Books");
+format_option_type = OptionType.find_or_create_by_name_and_presentation("Format","Format")
+
+last_row_id = nil
+last_p = nil
+
+FasterCSV.foreach(Rails.root.join( "/home/mredd/gk/gkbooks/db/import/osc_export.csv")) do |row|
+  if row[0] == last_row_id # set up product variant
+    v = Variant.create :product => last_p, :price => row[7]
+    v.option_values = [OptionValue.find_or_create_by_name_and_presentation_and_option_type_id(row[6], row[6], format_option_type.id)]
+    v.save
+  else # new product
+    p = Product.create :name => row[1], :price => row[4], :description => row[2]
+    p.available_on = Time.now
+    p.taxons << Taxon.find_or_create_by_name_and_taxonomy_id(row[8], prod_taxonomy.id)
+    p.option_types = [format_option_type]
+    p.save
+
+    if row[6] != 'NULL'
+      v = Variant.create :product => p, :price => row[4]
+      v.option_values = [OptionValue.find_or_create_by_name_and_presentation_and_option_type_id(row[6], row[6], format_option_type.id)]
+      v.save
+    end
+
+  end
+  last_row_id = row[0]
+  last_p = p.nil? ? last_p : p
+end
+
