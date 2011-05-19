569f2f6729f5bf93f27947693312cc6de3520def
Matthew Redd <matthew.redd@gmail.com>
2011-05-18 23:37:28 -0700

    first stab at rotator
    	new file:   app/views/shared/_head.html.erb
    	new file:   app/views/shared/_homepage_rotator.html.erb
    	modified:   lib/site_hooks.rb
    	new file:   public/javascripts/jquery.cycle.all.min.js
    	modified:   public/stylesheets/screen.css

--- lib/site_hooks.rb
+++ lib/site_hooks.rb
@@ -1,5 +1,6 @@
   class SiteHooks < Spree::ThemeSupport::HookListener
     #replace :sidebar, 'shared/gk_nav'
     remove :sidebar
+    insert_before :homepage_products, 'shared/homepage_rotator'
 
   end
