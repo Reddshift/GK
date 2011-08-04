4b1cef862bd5c079543f9b32ca9b6d1e3c9ab5d9
Matthew Redd <matthew.redd@gmail.com>
2011-08-03 22:05:13 -0700

    testing 	modified:   app/models/gateway/elavon.rb

--- app/models/gateway/elavon.rb
+++ app/models/gateway/elavon.rb
@@ -4,6 +4,7 @@ class Gateway::Elavon < Gateway
 
   def provider_class
     ActiveMerchant::Billing::ElavonGateway
+    puts "I was here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   end
 
 end
