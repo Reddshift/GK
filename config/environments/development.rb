12a4746b70871dfd5d170ad18e6d6960f48ca7a9
Matthew Redd <matthew.redd@gmail.com>
2012-02-05 23:41:57 -0800

    Commit right before running migrations for 0.70.3
    	modified:   Gemfile
    	modified:   Gemfile.lock
    	new file:   app/assets/javascripts/admin/all.js
    	new file:   app/assets/javascripts/store/all.js
    	new file:   app/assets/stylesheets/admin/all.css
    	new file:   app/assets/stylesheets/store/all.css
    	modified:   config/application.rb
    	modified:   config/database.yml
    	modified:   config/environment.rb
    	modified:   config/environments/development.rb
    	new file:   config/spree.yml
    	new file:   db/migrate/20100323182121_create_newsletter_subscriptions.rb
    	new file:   db/migrate/20120206073713_create_activators.rb
    	new file:   db/migrate/20120206073714_eligible_for_adjustments.rb
    	new file:   db/migrate/20120206073715_promotion_changes_to_subclass_of_activator.rb
    	new file:   db/migrate/20120206073716_create_promotion_actions.rb
    	new file:   db/migrate/20120206073717_create_promotion_action_line_items.rb
    	modified:   db/seeds.rb
    	modified:   public/robots.txt

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Gkdemo::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
+  # deprecated in rails 3.1 #config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
