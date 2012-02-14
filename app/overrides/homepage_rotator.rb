6beec56545326d7f732467d09c67b7b31d193dcf
Matthew Redd <matthew.redd@gmail.com>
2012-02-14 13:55:04 -0800

    fix rotator showing up on all pages
    	modified:   app/overrides/homepage_rotator.rb
    	modified:   app/views/layouts/spree_application.html.erb

--- app/overrides/homepage_rotator.rb
+++ app/overrides/homepage_rotator.rb
@@ -1,5 +1,6 @@
-Deface::Override.new(:virtual_path  => "layouts/spree_application",
-            :replace => "div#rotator[data-hook]",
+Deface::Override.new(:virtual_path  => "home/index",
+            #:replace => "div#rotator[data-hook]",
+            :insert_before => "[data-hook='homepage_products']",
             :partial   => '../views/shared/homepage_rotator',
             :name         => "homepage_rotator")
 
