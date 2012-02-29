99c1bce2a5aa818693f1ab373da55b7fa466b0e3
Matthew Redd <matthew.redd@gmail.com>
2012-02-29 06:25:43 -0800

    Theses files no longer used in new version of spree
    	deleted:    lib/site_hooks.rb
    	deleted:    lib/spree_site.rb

--- lib/site_hooks.rb
+++
@@ -1,7 +0,0 @@
-  class SiteHooks < Spree::ThemeSupport::HookListener
-    #replace :sidebar, 'shared/gk_nav'
-    remove :sidebar
-    insert_before :homepage_products, 'shared/homepage_rotator'
-    replace :footer_right, 'shared/footer_gk'
-
-  end
